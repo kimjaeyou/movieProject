ac696e26f4a6015719c8125c2f30f333383dec7b
kimjaeyou <unknown88455144@gmail.com>
2023-12-04 21:35:09 +0900

    end

---
+++ src/main/java/movieComm/MovieProjectApplication.java
@@ -0,0 +1,13 @@
+package movieComm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieProjectApplication.class, args);
+	}
+
+}
