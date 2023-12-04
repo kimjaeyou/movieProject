89dba58ebcf655d8000cb5f2c5b6b79d0cd9b428
kimjaeyou <unknown88455144@gmail.com>
2023-12-04 20:22:53 +0900

    code add

---
+++ src/test/java/stock_m/MovieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package stock_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
